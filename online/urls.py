e787d33b4cb31e31326a83751ac1a74591b356a4
Chebichii-Lab <chebichiiserem@gmail.com>
2023-06-24 09:37:45 +0300

    create urls.py file to link the view functions with each template file

---
+++ online/urls.py
@@ -0,0 +1,26 @@
+from django.urls import path
+from . import views
+from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
+from django.conf import settings
+
+
+
+urlpatterns=[
+    path(r'^$',views.index,name='index'),
+    # url('register/',views.signup, name='registration'),
+    # url('login/', auth_views.LoginView.as_view(), name='login'),
+    # url('logout/',auth_views.LogoutView.as_view(), name='logout'),
+    # url('profile/', views.user_profile, name='profile'),
+    # path('list/', views.polls_list, name='list'),
+    # path('add/', views.polls_add, name='add'),
+    # path('edit/<int:poll_id>/', views.polls_edit, name='edit'),
+    # path('delete/<int:poll_id>/', views.polls_delete, name='delete_poll'),
+    # path('edit/<int:poll_id>/choice/add/', views.add_choice, name='add_choice'),
+    # path('edit/choice/<int:choice_id>/', views.choice_edit, name='choice_edit'),
+    # path('delete/choice/<int:choice_id>/',views.choice_delete, name='choice_delete'),
+    # path('<int:poll_id>/vote/', views.poll_vote, name='vote'),
+    # path('<int:poll_id>/', views.poll_detail, name='detail'),
+    # path('end/<int:poll_id>/', views.endpoll, name='end_poll'),
+    # path('list/user/', views.list_by_user, name='list_by_user'),
+]
