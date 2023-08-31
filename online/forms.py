a7a5747cdcdee3576bed716f178e1148860ac98b
Chebichii-Lab <chebichiiserem@gmail.com>
2023-08-31 07:33:13 +0300

    edit the add poll form

--- online/forms.py
+++ online/forms.py
@@ -20,12 +20,12 @@ class UserProfileForm(forms.ModelForm):
 
 class AddPollForm(forms.ModelForm):
 
-    choice1 = forms.CharField(label='Choice 1', max_length=100, min_length=1, widget=forms.TextInput(attrs={'class': 'form-control'}))
-    choice2 = forms.CharField(label='Choice 2', max_length=100, min_length=1, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    # choice1 = forms.CharField(label='Choice 1', max_length=100, min_length=1, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    # choice2 = forms.CharField(label='Choice 2', max_length=100, min_length=1, widget=forms.TextInput(attrs={'class': 'form-control'}))
 
     class Meta:
         model = Poll
-        fields = ['description','choice1', 'choice2']
+        fields = ['description']
         widgets = {
             'text': forms.Textarea(attrs={'class': 'form-control', 'rows': 5, 'cols': 20}),
         }
